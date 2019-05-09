d9c6f4d0bb62bdbd4d1eca8be044df9c77ade3bb
Tanmay17 <tanmays321@gmail.com>
2019-05-09 11:08:25 +0530

    Database Setuped with conditions

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const app = require('express')();
 var http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
+const uuidv1 = require('uuid/v1');
 const sqlite3 = require('sqlite3').verbose();
 
 //DB setup
-var db = new sqlite3.Database('./Database/relation.db', sqlite3.OPEN_READWRITE, (err)=>{
+var db = new sqlite3.Database('./Database/relation.db', (err)=>{
     if(err) console.error(err.message);
     console.log("Connection with in-memory DB is succesfully made");
+    //Creating Table on start of server
+    db.serialize(()=> {
+        db.run('CREATE TABLE IF NOT EXISTS relations (id TEXT, ip TEXT, num TEXT, roman TEXT)');
+    });
 });
 
 //Creating Routes
 app.get('/', (req, res) => {
     
-    db.serialize(()=> {
-        // db.run('CREATE TABLE relations (num TEXT, roman TEXT)');
-        var stmt = db.prepare("INSERT into relations VALUES ('1','I')");
-        console.log(stmt)
-        stmt.finalize();
-        db.each("SELECT * FROM relations", function(err, row) {
-            console.log(row. err);
-        });
-    });
+    // db.run("INSERT into relations VALUES (?,?,?)", ['2','IP','3'], (err)=>{
+    //     if(err) console.log(err.message)
+    // });
+    
     res.sendfile(__dirname + '/index.html');
 }); 
 
 //IO Connections
 io.on('connection', (socket) => {
     console.log('A User connected');
+
+    let address = socket.handshake.address;
+    let getIP = (address)=> {
+        return address.slice(7);
+    }
+    console.log(getIP(address))
     //Event
     socket.on('chat message', (msg) => {
 
@@ -71,17 +77,55 @@ io.on('connection', (socket) => {
         }
 
         let response;
-        if(msg.match(/^[0-9]+$/) != null)
-            response = intToRoman(msg);
-        else 
-            response = romanToInt(msg);
-        
-        //Broadcasting msg to all clients in socket
-        if(response == -1)
-            io.emit('chat message', msg);
-        else
-            io.emit('chat message', response);
+        if(msg.match(/==/g)){
+            //if int
+            if(msg.charAt(0).match(/^[0-9]+$/) != null){
+                db.all("SELECT * FROM relations WHERE ip=? AND num=?", [getIP(address), msg.slice(0, msg.indexOf('=') )], (err, rows)=> {
+                    //Replace Values
+                    rows.forEach((row)=>{
+                        db.run("DELETE FROM relations WHERE id=?", [row.id], (err)=>{
+                            if(err) console.log(err.message);
+                        });
+                    });
+                });
+                db.run("INSERT into relations VALUES (?,?,?,?)", [uuidv1(), ip, msg.slice(0, msg.indexOf('=') ), /*romanValue*/], (err)=>{
+                    if(err) console.log(err.message);
+                    console.log("New relation Added");
+                }); 
+            }else{
+                db.all("SELECT * FROM relations WHERE ip=? AND roman=?", [getIP(address), msg.slice(0, msg.indexOf('=') )], (err, rows)=> {
+                    //Replace Values
+                    rows.forEach((row)=>{
+                        db.run("DELETE FROM relations WHERE id=?", [row.id], (err)=>{
+                            if(err) console.log(err.message);
+                        });
+                    });
+                });
+                db.run("INSERT into relations VALUES (?,?,?,?)", [uuidv1(), ip, /*numericalValue*/, msg.slice(0, msg.indexOf('=') )], (err)=>{
+                    if(err) console.log(err.message);
+                    console.log("New relation Added");
+                }); 
+            }
+        }else{
+            //Geting all the data
+            if(msg.match(/^[0-9]+$/) != null){
+                db.all("SELECT * FROM relations WHERE ip=? AND num=?", [getIP(address), msg], (err, rows)=> {
+                    io.emit('chat message', rows.roman); 
+                });
+                response = intToRoman(msg);
+            }else{
+                db.all("SELECT * FROM relations WHERE ip=? AND roman=?", [getIP(address), msg], (err, rows)=> {
+                    io.emit('chat message', rows.num); 
+                });
+                response = romanToInt(msg);
+            }
 
+            //Broadcasting msg to all clients in socket
+            if(response == -1)
+                io.emit('chat message', msg);
+            else
+                io.emit('chat message', response);  
+        }
     });
 
     socket.on('disconnect', ()=>{
